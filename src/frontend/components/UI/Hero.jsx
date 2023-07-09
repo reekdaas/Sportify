6fa029e1d8d42fe904a4d638609b3d7b5f995833
Rittik Das <dassrittik1998@gmail.com>
2023-07-09 14:38:20 +0530

    Update

--- src/frontend/components/UI/Hero.jsx
+++ src/frontend/components/UI/Hero.jsx
@@ -6,20 +6,13 @@ export default function Hero() {
   const navigate = useNavigate();
   return (
     <div>
-      {/* <article className={styles.content}>
-        <h1>Welcome To SportiFy</h1>
-        <p>One stop solution for your sporting needs.</p>
-        <Link to="/products" className="btn ">
-          shop now
-        </Link>
-      </article> */}
       <div className={styles.imgContainer}>
         <img src={heroImage} className={styles.mainImg} alt="heroImage" />
       </div>
       <div className={styles.heroOverlay}></div>
       <article className={styles.heroContainer}>
         <div className={styles.heroContent}>
-          <h1>Welcome To SportiFy</h1>
+          <h1>Welcome To SporTiFy</h1>
           <button
             onClick={() => {
               navigate("/products");
