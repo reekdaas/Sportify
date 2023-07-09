f7650eb15502d8b0fb013845bd84f03ef3b115e4
Rittik Das <dassrittik1998@gmail.com>
2023-07-09 14:37:40 +0530

    Update

--- src/frontend/components/product/Stars/Stars.jsx
+++ src/frontend/components/product/Stars/Stars.jsx
@@ -1,10 +1,11 @@
 import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
+import styles from "./star.module.css";
 
 export default function Stars({ rating }) {
   const arrOfStars = Array.from({ length: 5 }, (_, index) => {
     const number = index + 0.5;
     return (
-      <span key={index}>
+      <span className={styles.star} key={index}>
         {rating > number ? (
           <BsStarFill />
         ) : rating > index ? (
