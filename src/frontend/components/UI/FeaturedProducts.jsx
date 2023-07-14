cbce5a01d5afb175c2050887db158c5c61e476b5
Rittik Das <dassrittik1998@gmail.com>
2023-07-15 01:02:26 +0530

    Update FeaturedProducts.jsx

--- src/frontend/components/UI/FeaturedProducts.jsx
+++ src/frontend/components/UI/FeaturedProducts.jsx
@@ -2,23 +2,28 @@ import React from "react";
 import styles from "./featuredProduct.module.css";
 import { useProductContext } from "../../context";
 import CardComponent from "../card/cardComponent";
+import Spinner from "../spinner/spinner";
 
 export default function FeaturedProducts() {
-  const { productState, productDispatch } = useProductContext();
-  const {
-    product_loading: loading,
-    products_error: error,
-    featured_products: featured,
-  } = productState;
+  const { productState, loading } = useProductContext();
+  const { featured_products: featured } = productState;
   // console.log(featured);
   return (
     <div className={styles.featuredContainer}>
-      <h1 className={styles.featuredHeading}>Featured Products</h1>
-      <div className={styles.featuredCards}>
-        {featured?.map((product) => (
-          <CardComponent key={product?._id} product={product} />
-        ))}
-      </div>
+      {loading ? (
+        <div className={styles.spinner}>
+          <Spinner />
+        </div>
+      ) : (
+        <>
+          <h1 className={styles.featuredHeading}>Featured Products</h1>
+          <div className={styles.featuredCards}>
+            {featured?.map((product) => (
+              <CardComponent key={product?._id} product={product} />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
