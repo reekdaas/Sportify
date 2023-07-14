c8f024de84f5b07911098f02e3b770aba4b7f360
Rittik Das <dassrittik1998@gmail.com>
2023-07-14 22:49:25 +0530

    Refractor

--- src/frontend/pages/wishlist/wishList.jsx
+++ src/frontend/pages/wishlist/wishList.jsx
@@ -1,33 +1,38 @@
 import CardComponent from "../../components/card/cardComponent";
+import Spinner from "../../components/spinner/spinner";
 import { useWishListContext } from "../../context";
-import { isProductAlreadyInWishList } from "../../utils/getProductUtils";
 import styles from "./wishList.module.css";
 
-// const wishListProduct = [];
-
 export default function WishListPage() {
-  const { wishlistState } = useWishListContext();
+  const { wishlistState, loading } = useWishListContext();
   const wishListProduct = wishlistState?.wishlist;
-  console.log(wishlistState?.wishlist);
-
   return (
     <div className={styles.wishlistContainer}>
-      {wishListProduct.length === 0 && (
-        <div className={styles.emptyWishlist}>
-          <h1>Wishlist is empty!!!!</h1>
+      {loading ? (
+        <div className={styles.spinner}>
+          {" "}
+          <Spinner />
         </div>
-      )}
-
-      {wishListProduct.length > 0 && (
+      ) : (
         <>
-          <div className={styles.wishlistHeading}>
-            <h1>Total Wishlist Items: {wishListProduct.length}</h1>
-          </div>
-          <div className={styles.wishlistCardsContainer}>
-            {wishListProduct.map((product) => (
-              <CardComponent product={product} />
-            ))}
-          </div>
+          {wishListProduct.length === 0 && (
+            <div className={styles.emptyWishlist}>
+              <h1>Wishlist is empty!!!!</h1>
+            </div>
+          )}
+
+          {wishListProduct.length > 0 && (
+            <>
+              <div className={styles.wishlistHeading}>
+                <h1>Total Wishlist Items: {wishListProduct.length}</h1>
+              </div>
+              <div className={styles.wishlistCardsContainer}>
+                {wishListProduct.map((product) => (
+                  <CardComponent product={product} />
+                ))}
+              </div>
+            </>
+          )}
         </>
       )}
     </div>
