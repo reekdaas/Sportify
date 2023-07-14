b41209f1a412bf28388ec90d822506ddc1f4ee14
Rittik Das <dassrittik1998@gmail.com>
2023-07-15 01:01:41 +0530

    Update FeaturedCategory.jsx

--- src/frontend/components/UI/FeaturedCategory.jsx
+++ src/frontend/components/UI/FeaturedCategory.jsx
@@ -1,12 +1,13 @@
 import React, { useContext } from "react";
 import styles from "./featuredCategory.module.css";
-import { ProductsContext } from "../../context/products/productsProvider";
+import { useProductContext } from "../../context/products/productsProvider";
 import { Link } from "react-router-dom";
 import { useFilterContext } from "../../context";
+import Spinner from "../spinner/spinner";
 
 export default function FeaturedCategory() {
   const { filterProductDispatch } = useFilterContext();
-  const { productState } = useContext(ProductsContext);
+  const { productState, loading } = useProductContext();
   const handleCLick = (categoryName) => {
     // console.log(categoryName);
     filterProductDispatch({ type: "CLEAR_FILTERS" });
@@ -18,22 +19,30 @@ export default function FeaturedCategory() {
 
   return (
     <div className={styles.featuredSection}>
-      <h2 className={styles.featuredHeading}>Explore Categories</h2>
-      <div className={styles.featuredContainer}>
-        {productState?.categoriesList?.map(({ _id, categoryName }) => (
-          <Link
-            to="/products"
-            key={_id}
-            onClick={() => {
-              handleCLick(categoryName);
-            }}
-          >
-            <div className={styles.featureCard}>
-              <h1 className={styles.featureCardTitle}>{categoryName}</h1>
-            </div>
-          </Link>
-        ))}
-      </div>
+      {loading ? (
+        <div className={styles.spinner}>
+          <Spinner />
+        </div>
+      ) : (
+        <>
+          <h2 className={styles.featuredHeading}>Explore Categories</h2>
+          <div className={styles.featuredContainer}>
+            {productState?.categoriesList?.map(({ _id, categoryName }) => (
+              <Link
+                to="/products"
+                key={_id}
+                onClick={() => {
+                  handleCLick(categoryName);
+                }}
+              >
+                <div className={styles.featureCard}>
+                  <h1 className={styles.featureCardTitle}>{categoryName}</h1>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
