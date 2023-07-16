fffad55102677d7c12ce415cc8e4155108471c68
Rittik Das <dassrittik1998@gmail.com>
2023-07-16 21:58:01 +0530

    Refractor

--- src/frontend/pages/products/productListing.jsx
+++ src/frontend/pages/products/productListing.jsx
@@ -3,31 +3,55 @@ import { useFilterContext, useProductContext } from "../../context";
 import getFilteredProducts from "../../utils/getFilteredProducts";
 import styles from "./productList.module.css";
 import CardComponent from "../../components/card/cardComponent";
+import pagination from "../../utils/pagination";
+import { useState } from "react";
+import PaginationBtn from "../../components/paginationBtn/paginationBtn";
+import Spinner from "../../components/spinner/spinner";
 
 export default function ProductListing() {
   // const { state } = useLocation();
+  const [pageNumber, setPageNUmnber] = useState(0);
 
-  const { filterProductState } = useFilterContext();
+  const { filterProductState, loading } = useFilterContext();
   const { productState } = useProductContext();
 
   const filteredProducts = getFilteredProducts(
     productState?.productList,
     filterProductState
   );
-
-  // console.log(filteredProducts);
-  // // console.log(productState);
-  // if (filterProductState.length === 0) {
-  // }
+  const products = pagination(filteredProducts);
+  const productsPerPage = products[pageNumber];
 
   return (
     <main className={styles.mainContainer}>
-      <Filter />
-      <div className={styles.container}>
-        {filteredProducts?.map((filteredProduct) => (
-          <CardComponent product={filteredProduct} key={filteredProduct?._id} />
-        ))}
-      </div>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Filter />
+          <div className={styles.container}>
+            <div className={styles.cardContainer}>
+              {" "}
+              {productsPerPage?.map((filteredProduct) => (
+                <CardComponent
+                  product={filteredProduct}
+                  key={filteredProduct?._id}
+                />
+              ))}
+            </div>
+            {!loading ? (
+              <div className={styles.btnContainer}>
+                {" "}
+                <PaginationBtn
+                  products={products}
+                  handlePage={setPageNUmnber}
+                  pageNumber={pageNumber}
+                />
+              </div>
+            ) : null}
+          </div>
+        </>
+      )}
     </main>
   );
 }
