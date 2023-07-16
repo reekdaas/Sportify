6d396c02ae76e8c5a87c11f837d7b9b7f68147c5
Rittik Das <dassrittik1998@gmail.com>
2023-07-16 21:59:26 +0530

    Update wishListService.js

--- src/frontend/services/wishlistService/wishListService.js
+++ src/frontend/services/wishlistService/wishListService.js
@@ -1,65 +1,20 @@
 import axios from "axios";
 
-export const getWishlistService = async (encodedToken, wishlistDispatch) => {
-  try {
-    const response = await axios.get("/api/user/wishlist", {
-      headers: { authorization: encodedToken },
-    });
-    const {
-      status,
-      data: { wishlist },
-    } = response;
-    if (status === 200) {
-      wishlistDispatch({ type: "DISPLAY_WISHLIST", payload: wishlist });
-    }
-  } catch (err) {
-    console.error(err);
-  }
-};
+export const getWishlistService = async (encodedToken) =>
+  await axios.get("/api/user/wishlist", {
+    headers: { authorization: encodedToken },
+  });
 
-export const addToWishlistService = async (
-  encodedToken,
-  product,
-  wishlistDispatch
-) => {
-  try {
-    const response = await axios.post(
-      "/api/user/wishlist",
-      {
-        product,
-      },
-      { headers: { authorization: encodedToken } }
-    );
-    const {
-      status,
-      data: { wishlist },
-    } = response;
-    if (status === 201) {
-      wishlistDispatch({ type: "ADD_TO_WISHLIST", payload: wishlist });
-    }
-  } catch (err) {
-    console.error(err);
-  }
-};
+export const addToWishlistService = async (encodedToken, product) =>
+  await axios.post(
+    "/api/user/wishlist",
+    {
+      product,
+    },
+    { headers: { authorization: encodedToken } }
+  );
 
-export const removeFromWishlistService = async (
-  encodedToken,
-  productId,
-  wishlistDispatch
-) => {
-  try {
-    const response = await axios.delete(`/api/user/wishlist/${productId}`, {
-      headers: { authorization: encodedToken },
-    });
-    // console.log(response);
-    const {
-      status,
-      data: { wishlist },
-    } = response;
-    if (status === 200) {
-      wishlistDispatch({ type: "REMOVE_FROM_WISHLIST", payload: wishlist });
-    }
-  } catch (err) {
-    console.error(err);
-  }
-};
+export const removeFromWishlistService = async (encodedToken, productId) =>
+  await axios.delete(`/api/user/wishlist/${productId}`, {
+    headers: { authorization: encodedToken },
+  });
