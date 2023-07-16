7428c017a88e2032ff5721a52aa0a3854e5e0ba7
Rittik Das <dassrittik1998@gmail.com>
2023-07-16 21:56:40 +0530

    Update productsProvider.js

--- src/frontend/context/products/productsProvider.js
+++ src/frontend/context/products/productsProvider.js
@@ -5,32 +5,67 @@ import {
   useReducer,
   useState,
 } from "react";
-import { productStateReducer } from "../../reducer/productsStateReducer";
-import { fetchProductList } from "../../services/fetchData/fetchProduct";
-import { fetchCategories } from "../../services/fetchData/fetchCategories";
+import axios from "axios";
+import {
+  initialProductState,
+  productStateReducer,
+} from "../../reducer/productsStateReducer";
 
 export const ProductsContext = createContext(null);
 
 export default function ProductsContextProvider({ children }) {
-  const initialProductState = {
-    productList: [],
-    categoriesList: [],
-    product_loading: false,
-    products_error: false,
-    featured_products: [],
-  };
-
+  const [loading, setLoading] = useState(false);
   const [productState, productDispatch] = useReducer(
     productStateReducer,
     initialProductState
   );
+  const fetchAllProducts = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("/api/products");
+      // console.log(response);
+      const {
+        data: { products },
+        status,
+      } = response;
+      if (status === 200) {
+        productDispatch({
+          type: "ADD_PRODUCT_LIST",
+          payload: products,
+        });
+      }
+    } catch (err) {
+      // console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const fetchAllCategory = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("/api/categories");
+      const {
+        data: { categories },
+        status,
+      } = response;
+      if (status === 200) {
+        productDispatch({
+          type: "ADD_CATEGORIES",
+          payload: categories,
+        });
+      }
+    } catch (err) {
+      // console.log(err);
+    }
+    setLoading(false);
+  };
 
   useEffect(() => {
-    fetchProductList(productDispatch);
-    fetchCategories(productDispatch);
+    fetchAllProducts();
+    fetchAllCategory();
   }, []);
 
-  const value = { productState, productDispatch };
+  const value = { productState, productDispatch, loading };
   return (
     <ProductsContext.Provider value={value}>
       {children}
