96f292b66b17a1a8696449f0db81a7369dcc647b
Rittik Das <dassrittik1998@gmail.com>
2023-07-16 21:58:35 +0530

    Update productsStateReducer.js

--- src/frontend/reducer/productsStateReducer.js
+++ src/frontend/reducer/productsStateReducer.js
@@ -1,21 +1,21 @@
+export const initialProductState = {
+  productList: [],
+  categoriesList: [],
+  featured_products: [],
+};
+
 export const productStateReducer = (state, { type, payload }) => {
   switch (type) {
-    case "FETCHING_BEGINS":
-      return { ...state, product_loading: true };
     case "ADD_PRODUCT_LIST": {
       const featuredProduct = payload.filter(({ isFeatured }) => isFeatured);
       return {
         ...state,
         productList: [...payload],
-        product_loading: false,
         featured_products: featuredProduct,
       };
     }
-
     case "ADD_CATEGORIES":
-      return { ...state, categoriesList: payload, product_loading: false };
-    case "ERROR":
-      return { ...state, product_loading: false, products_error: true };
+      return { ...state, categoriesList: payload };
 
     default:
       return { ...state };
