ff26144f6f8d43e20cbd18728bdb4182a62a11c7
Rittik Das <dassrittik1998@gmail.com>
2023-07-16 21:59:03 +0530

    Refractor

--- src/frontend/services/fetchData/fetchProduct.js
+++ src/frontend/services/fetchData/fetchProduct.js
@@ -1,15 +1,1 @@
-import axios from "axios";
 
-export const fetchProductList = async (productDispatch) => {
-  try {
-    const res = await axios.get("/api/products");
-    if (res.status === 200) {
-      productDispatch({
-        type: "ADD_PRODUCT_LIST",
-        payload: res?.data?.products,
-      });
-    }
-  } catch (e) {
-    console.log(e.error);
-  }
-};
