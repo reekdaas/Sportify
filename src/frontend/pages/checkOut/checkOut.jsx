90aaed021d40e3138f01b429bea8e61fbdc3ab20
Rittik Das <dassrittik1998@gmail.com>
2023-07-16 21:57:16 +0530

    Refractor

--- src/frontend/pages/checkOut/checkOut.jsx
+++ src/frontend/pages/checkOut/checkOut.jsx
@@ -1,14 +1,12 @@
-import { useState } from "react";
 import CheckOutCard from "../../components/checkOut/CheckOutCard";
 import AddressCard from "../../components/checkOut/addressCard";
-import { useAddressContext } from "../../context";
 import styles from "./checkout.module.css";
 export default function CheckOut() {
   // const [selectedAddress, setSelectedAddress] = useState();
 
   return (
     <div className={styles.checkoutPage}>
-      <h1 className={styles.checkoutHeader}>CheckOut</h1>
+      <h1 className={styles.checkoutHeader}>Checkout</h1>
 
       <div className={styles.checkoutContent}>
         <AddressCard />
