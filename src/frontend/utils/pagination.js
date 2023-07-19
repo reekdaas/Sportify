8219c3baa622f02c23a516633d28c30b24531a04
Rittik Das <dassrittik1998@gmail.com>
2023-07-19 22:57:14 +0530

    Update pagination.js

--- src/frontend/utils/pagination.js
+++ src/frontend/utils/pagination.js
@@ -1,3 +1,5 @@
+//! This function converts the array of all products into nested array based on items present in per page.It accepts the all products array as parameter and returns the nested array
+
 export default function pagination(productData) {
   const itemsperpage = 6;
   const noOfPages = Math.ceil(productData.length / itemsperpage);
