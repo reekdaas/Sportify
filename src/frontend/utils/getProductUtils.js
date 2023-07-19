13338134394243a8b4364d10d9068213ec02e726
Rittik Das <dassrittik1998@gmail.com>
2023-07-19 22:57:10 +0530

    Update getProductUtils.js

--- src/frontend/utils/getProductUtils.js
+++ src/frontend/utils/getProductUtils.js
@@ -1,10 +1,16 @@
+//! This function checks whether the product is already present in wishlist or not
+
 export const isProductAlreadyInWishList = (productId, allProducts) => {
   return allProducts.some(({ _id }) => _id === productId);
 };
 
+//! This function checks whether the product is already present in cart or not
+
 export const isProductAlreadyInCart = (productId, allProducts) => {
   return allProducts.some(({ _id }) => _id === productId);
 };
+
+// ! This function helps to get total cart amount
 
 export const getTotalCartAmount = (cart) => {
   return cart.reduce((acc, cur) => acc + cur?.price * cur.qty, 0);
