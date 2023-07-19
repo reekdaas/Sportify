5db9fd737d8dde80ac43d96e7e8b39b9ba0f5f1d
Rittik Das <dassrittik1998@gmail.com>
2023-07-19 22:56:56 +0530

    Update requiredAuth.js

--- src/frontend/protectedRoute/requiredAuth.js
+++ src/frontend/protectedRoute/requiredAuth.js
@@ -3,8 +3,9 @@ import { useAuthContext } from "../context";
 
 export default function RequiredAuth({ children }) {
   const { token } = useAuthContext();
-  // console.log(token);
-  // console.log(token);
+
+  //! If user is already logged in i.e token is present then the children component get rendered, otherwise it redirects the user to login page with the help of Navigate component
+
   const location = useLocation();
   return token ? children : <Navigate to="/login" state={{ from: location }} />;
 }
